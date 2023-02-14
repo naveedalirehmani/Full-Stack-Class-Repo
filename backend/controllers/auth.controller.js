48b1d76226f676fb14ed0901c61530944655b5a6
zeeshooo <zeeshan@invyce.com>
2023-02-14 17:37:30 +0500

    adding password encryption apis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,34 +1,66 @@
 const UserModel = require("../schemas/user.schema");
+const bcrypt = require("bcryptjs");
+const { HashPassword, VerifyPassword } = require("../helpers");
 
 /**
  *
  * @param {*} req
  * @param {*} res
  * @returns
- */
+
+*/
 
 const Signup = async (req, res) => {
-  const { username, email, password, confirm_password } = req.body;
+  try {
+    const { username, email, password, confirm_password } = req.body;
 
-  console.log("test");
-  const user = new UserModel({
-    username,
-    email,
-    password,
-    confirm_password,
-  });
+    if (password === confirm_password) {
+      const user = new UserModel({
+        username,
+        email,
+        password: HashPassword(password),
+      });
+      await user.save();
 
-  await user.save();
-
-  return res.status(201).json({
-    message: "Signup successfull",
-    status: true,
-    user,
-  });
+      return res.status(201).json({
+        message: "Signup successfull",
+        status: true,
+        user,
+      });
+    } else {
+      return res.status(400).send({
+        message: "Password and confirm password miss match",
+      });
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const Login = async (req, res) => {
-  res.send("login route");
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({
+    $or: [
+      {
+        username: username,
+      },
+      { email: username },
+    ],
+  });
+
+  if (VerifyPassword(password, user.password)) {
+    return res.status(200).json({
+      message: "Login successfull",
+      user,
+    });
+  } else {
+    return res.status(400).send({
+      message: "Password incorrect",
+    });
+  }
+
+  console.log(user);
 };
 
 module.exports = {
